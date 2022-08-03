c50709a06738e1be7fa74a00c585aad289ccaef2
leec89 <leec89@gmail.com>
2022-08-02 22:33:40 -0500

    feat: Got sendgrid email service configured and working

--- src/main/java/com/example/springblog/services/EmailService.java
+++ src/main/java/com/example/springblog/services/EmailService.java
@@ -1,34 +1,42 @@
 package com.example.springblog.services;
 
+import com.sendgrid.*;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.*;
+
 import com.example.springblog.model.Post;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service("emailService")
 
 public class EmailService {
 
-    @Autowired
-    public JavaMailSender emailSender;
-
-    @Value("${spring.mail.from}")
-    private String from;
+    @Value("${spring.sendgrid.api-key}")
+    private String sendgridApiKey;
 
     public void prepareAndSend(Post post, String subject) {
-        SimpleMailMessage msg = new SimpleMailMessage();
-        msg.setFrom(from);
-        msg.setTo(post.getUser().getEmail());
-        msg.setSubject(subject);
-        String body = "Thank you for posting to this SpringBlog! The title of your post submitted was [" + post.getTitle() + "]. If this was not you, please contact customer support.";
-        msg.setText(body);
+        Email from = new Email("leec89@protonmail.com");
+        Email to = new Email("leec89@gmail.com");
+        Content content = new Content("text/plain", "Thank you for posting to this SpringBlog! The title of your post submitted was [" + post.getTitle() + "]. If this was not you, please contact customer support.");
+        Mail mail = new Mail(from, subject, to, content);
+
+        SendGrid sg = new SendGrid(sendgridApiKey);
+        Request request = new Request();
 
         try {
-            this.emailSender.send(msg);
-        } catch(MailException mex) {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+            System.out.println(response.getStatusCode());
+            System.out.println(response.getBody());
+            System.out.println(response.getHeaders());
+
+        } catch(MailException | IOException mex) {
             System.out.println(mex.getMessage());
         }
     }
